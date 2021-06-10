e7bf501840cfdd7ce863038d3e7f0917dd66ebe3
Adam Shailer <adasha76@outlook.com>
2021-06-10 11:12:16 +0100

    Attempt to fix dependency vulnerability

--- dist/WrapChars.js
+++ dist/WrapChars.js
@@ -21,7 +21,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 /**
  * WrapChars Class - wrap inline letters/words in HTML elements.
  * @class WrapChars
- * @version 2.2.2
+ * @version 2.2.3
  * @author Adam Shailer <adasha76@outlook.com>
 */
 // eslint-disable-next-line no-unused-vars
