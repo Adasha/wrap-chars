797ad134ff74d5873ea605556fc2c270101d644a
Adasha <adasha76@outlook.com>
2026-07-28 16:20:54 +0100

    Refactoring test suite

--- tests/testsuite.js
+++ tests/testsuite.js
@@ -1,62 +1,59 @@
-(function(){
-    
-    document.getElementById('version_info').innerText = "version info to go here";
+
+import WrapChars from '../src/WrapCharsModule.mjs';
+document.getElementById('version_info').innerText = "version info to go here";
+alert('test');
+
+/*
+    * Markup handling
+    */
+
+var target = document.getElementById('target');
+var originalHTML = target.innerHTML;
+
+function restoreOriginalMarkup()
+{
+    target.innerHTML = originalHTML;
+}
 
 
-    /*
-     * Markup handling
-     */
+/*
+    * UI
+    */
 
-    var target = document.getElementById('target');
-    var originalHTML = target.innerHTML;
+var controlBlock = document.getElementById('controls');
 
-    function restoreOriginalMarkup()
-    {
-        target.innerHTML = originalHTML;
-    }
+var controls = {
+    selSplit: document.getElementById('sel_split'),
+    txtTag:   document.getElementById('txt_tag'),
+    txtClass: document.getElementById('txt_class'),
+    txtSkip:  document.getElementById('txt_skip'),
+    txtSpace: document.getElementById('txt_space'),
+    cbDeep:   document.getElementById('cb_deep'),
+    cbSpaces: document.getElementById('cb_spaces'),
+    btnApply: document.getElementById('btn_apply')
+};
 
+controls.btnApply.addEventListener('click', clickApply);
 
-    /*
-     * UI
-     */
+function clickApply(e)
+{
+    restoreOriginalMarkup();
 
-    var controlBlock = document.getElementById('controls');
+    params = {
+        split:       controls.selSplit.value,
+        tagName:     controls.txtTag.value,
+        className:   controls.txtClass.value,
+        skipClass:   controls.txtSkip.value,
+        spaceChar:   controls.txtSpace.value,
+        deep:        controls.cbDeep.checked,
+        wrapSpaces:  controls.cbSpaces.checked
 
-    var controls = {
-        selSplit: document.getElementById('sel_split'),
-        txtTag:   document.getElementById('txt_tag'),
-        txtClass: document.getElementById('txt_class'),
-        txtSkip:  document.getElementById('txt_skip'),
-        txtSpace: document.getElementById('txt_space'),
-        cbDeep:   document.getElementById('cb_deep'),
-        cbSpaces: document.getElementById('cb_spaces'),
-        btnApply: document.getElementById('btn_apply')
     };
 
-    controls.btnApply.addEventListener('click', clickApply);
+    WrapChars.wrap(target, params);
 
-    function clickApply(e)
-    {
-        restoreOriginalMarkup();
+    
+}
 
-        params = {
-            split:       controls.selSplit.value,
-            tagName:     controls.txtTag.value,
-            className:   controls.txtClass.value,
-            skipClass:   controls.txtSkip.value,
-            spaceChar:   controls.txtSpace.value,
-            deep:        controls.cbDeep.checked,
-            wrapSpaces:  controls.cbSpaces.checked
+controls.btnApply.dispatchEvent(new MouseEvent('click'));
 
-        };
-
-        WrapChars.wrap(target, params);
-
-        
-    }
-
-    controls.btnApply.dispatchEvent(new MouseEvent('click'));
-
-
-
-})();
